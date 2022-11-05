606001c04d51dc22a534a89a5e132e7af16b1ebc
Sampangi Charan Rajeev <108627841+Charanrajeev@users.noreply.github.com>
2022-11-06 01:59:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+function inputHandler(){
+    var x = document.getElementById('input').value;
+    // https://youtu.be/Z-jAdo4q5L0
+    for(let i = 0;i<x.length;i++)
+    {
+        if(x.charAt(i)=='.')
+        {
+        var results=x.slice(i+3,x.length)
+        // console.log(results)
+        }
+    }
+    var ele = document.createElement('iframe');
+    // ele.setAttribute('','560');
+ 
+    ele.setAttribute('src',`https://www.youtube.com/embed/${results}`);
+    
+    // title="YouTube video player"
+    var h =document.getElementById('height').value
+    var w = document.getElementById('width').value
+    ele.setAttribute('width',w)
+    ele.setAttribute('height',h)
+    var check = document.getElementById('check').checked
+    if(check==true)
+    {
+        ele.setAttribute('allowfullscreen','true')
+        
+    }
+    
+    
+    var parent = document.getElementById('videodisplay');
+    parent.appendChild(ele)
+    document.getElementById('input').value=''
+    
+    }
